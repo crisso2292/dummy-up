03664240221254722df5d9f7c90b8c4ed25b7d14
cristhiansoria <crisso2292@gmail.com>
2023-08-30 00:36:01 -0400

    first monorepo take 2

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,7 +3,7 @@ import { Button, Header } from "ui";
 export default function Page(): JSX.Element {
   return (
     <>
-      <Header text="Docs" />
+      <Header text="My Great Docs" />
       <Button />
     </>
   );
